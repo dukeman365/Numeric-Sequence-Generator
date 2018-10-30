e8a45e674beda955ff1753f4f3b6b5ecfb071daa
Robinson Cook <robinsoncook@gmail.com>
2018-10-30 14:05:30 -0500

    Initial commit

---
+++ SequenceGeneratorTests.java
@@ -0,0 +1,16 @@
+public class SequenceGeneratorTests {
+
+  public static boolean geometricSequenceGeneratorTest() {
+    return false;
+  }
+  
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    boolean error = false; 
+    
+    error = geometricSequenceGeneratorTest();
+    
+    
+  }
+
+}
